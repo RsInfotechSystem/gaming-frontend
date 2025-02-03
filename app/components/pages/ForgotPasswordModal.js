1bfdf9e419be9c24e22b32b5c2477cc26d9daa41
Abhishek <abhishektijare09@gmail.com>
2025-02-03 18:12:42 +0530

    pop up setup

--- app/components/pages/ForgotPasswordModal.js
+++ app/components/pages/ForgotPasswordModal.js
@@ -1,20 +1,28 @@
 "use client";
 import React from "react";
+import backarrow from "../../../public/homepage/backarrow.png";
+import Image from "next/image";
 
-export default function ForgotPasswordModal({ isOpen, onClose }) {
+export default function ForgotPasswordModal({ isOpen, onClose,openLoginModal }) {
     if (!isOpen) return null;
 
     return (
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
                 <div className="login_page_name">
+                    <button className="backarrow_btn" onClick={() => {
+                        onClose();
+                        setTimeout(openLoginModal, 300);
+                    }}>
+                        <Image src={backarrow} alt="backarrow" width={12}></Image>
+                    </button>
                     <p className="mb-0">RESET PASSWORD</p>
                 </div>
                 <hr className="modal-hr" />
                 <div className="p-5">
                     <input className="login_input" type="email" placeholder="Enter your Email" />
                     <div className="login_main mt-5">
-                        <button className="popup_btn">RESET PASSWORD</button>
+                        <button className="popup_btn">RESET</button>
                     </div>
                 </div>
             </div>
--- app/components/pages/SignUpModal.js
+++ app/components/pages/SignUpModal.js
@@ -1,22 +1,43 @@
 "use client";
 import React from "react";
+import backarrow from "../../../public/homepage/backarrow.png";
+import Image from "next/image";
 
-export default function SignUpModal({ isOpen, onClose }) {
+export default function SignUpModal({ isOpen, onClose, openLoginModal, openOtpModal }) {
     if (!isOpen) return null;
 
     return (
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
                 <div className="login_page_name">
-                    <p className="mb-0">CREATE ACCOUNT</p>
+                    <button className="backarrow_btn" onClick={()=>{
+                         onClose();
+                         setTimeout(openLoginModal, 300);
+                    }}>
+                        <Image src={backarrow} alt="backarrow" width={12}></Image>
+                    </button>
+                    <p className="mb-0">REGISTER</p>
                 </div>
                 <hr className="modal-hr" />
-                <div className="p-5">
-                    <input className="login_input" type="text" placeholder="Enter Name" />
-                    <input className="login_input" type="email" placeholder="Enter Email" />
-                    <input className="login_input" type="password" placeholder="Create Password" />
+                <div className="p-4">
+                    <input className="login_input" type="text" placeholder="Name" />
+                    <input className="login_input" type="email" placeholder="Email" />
+                    <input className="login_input" type="password" placeholder="Create Password" autoComplete="new-password" />
                     <div className="login_main mt-5">
-                        <button className="popup_btn">SIGN UP</button>
+                        <button className="popup_btn" onClick={() => {
+                            onClose();
+                            setTimeout(openOtpModal, 300);
+                        }}>NEXT</button>
+                    </div>
+                    <div className="mt-4">
+                        <p>
+                            <button className="text-link" onClick={() => {
+                                onClose();
+                                setTimeout(openLoginModal, 300);
+                            }}>
+                                Already had an account?
+                            </button>
+                        </p>
                     </div>
                 </div>
             </div>
