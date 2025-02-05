91101ab977aeea995d6ccee8b7995bb8f70e1a24
Abhishek <abhishektijare09@gmail.com>
2025-02-05 19:46:48 +0530

    leaderboard setup

--- app/dashboard/leaderboard/LeaderBoard.js
+++ app/dashboard/leaderboard/LeaderBoard.js
@@ -1,10 +1,33 @@
 // LeaderBoard.js
 "use client"
-import React from 'react';
+import React, { useState } from 'react';
 import network from '../../../public/dashboard/network.png';
 import Image from 'next/image';
 
 export default function LeaderBoard() {
+
+    const [roles, setRoles] = useState([
+        {
+            _id: "1",
+            role: "Admin",
+            tab: 40,
+            level: 5,
+        },
+        {
+            _id: "1",
+            role: "Admin",
+            tab: 30,
+            level: 5,
+
+        },
+        {
+            _id: "1",
+            role: "Admin",
+            tab: 34,
+            level: 3,
+
+        },
+    ])
     return (
         <>
             <div className="tournament_list">
@@ -21,10 +44,59 @@ export default function LeaderBoard() {
                             <i className="fas fa-search search_icon"></i>
                         </div>
                         <div className="add_btn_main">
-                           <button className='add_btn'>Add</button>
+                            <button className='add_btn'>Add</button>
                         </div>
                     </div>
-                   
+
+
+                    {/* table  */}
+                    <div className="table_wrapper">
+                        <div className="table_main">
+                            <div className="table_section">
+                                {/* Table Header */}
+                                <div className="table_header">
+                                    <div className="col_10p">
+                                        <h5>Rank</h5>
+                                    </div>
+                                    <div className="col_20p">
+                                        <h5>Team Name</h5>
+                                    </div>
+                                    <div className="col_60p">
+                                        <h5>Points</h5>
+                                    </div>
+                                   
+                                    <div className="col_10p">
+                                        <h5>Level</h5>
+                                    </div>
+                                </div>
+
+                                {/* Table Data */}
+                                {roles.length > 0 ? (
+                                    <>
+                                        {roles.map((roleDetails, index) => (
+                                            <div className="table_data" key={index}>
+                                                <div className="col_10p">
+                                                    <h6>{index + 1}</h6>
+                                                </div>
+                                                <div className="col_20p">
+                                                    <h6>{roleDetails?.role}</h6>
+                                                </div>
+                                                <div className="col_60p">
+                                                    <h6>{roleDetails?.tab}</h6>
+                                                </div>
+                                                <div className="col_10p">
+                                                    <h6>{roleDetails?.level}</h6>
+                                                </div>
+                                            </div>
+                                        ))}
+                                    </>
+                                ) : (
+                                    <p className="no_data">Data Not Available</p>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+
 
 
                 </div>
