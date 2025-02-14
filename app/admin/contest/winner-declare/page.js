a24486ca54a7cf91ceb01333721420515235130e
Abhishek <abhishektijare09@gmail.com>
2025-02-14 18:51:08 +0530

    winner declare

---
+++ app/admin/contest/winner-declare/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import WinnerDeclare from './WinnerDeclare'
+
+const page = () => {
+    return (
+        <>
+            <WinnerDeclare />
+        </>
+    )
+}
+
+export default page
