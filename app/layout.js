a664f5eaa5b20b2311e6cade073c3a3b60b961e7
Abhishek <abhishektijare09@gmail.com>
2025-02-16 11:06:47 +0530

    pages starts from top after changing tabs done

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style/dashboard.css";
 import { Providers } from "./provider";
+import ScrollToTop from "./ScrollToTop";
 // import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
@@ -23,11 +24,16 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Providers>{children}</Providers>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <Providers>
+          <ScrollToTop/>
+            {children}
+          
+          </Providers>
+        {/* <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> */}
       </body>
     </html>
   );
