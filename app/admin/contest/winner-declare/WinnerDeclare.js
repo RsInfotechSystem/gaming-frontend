c027c60fa47046663088f0a856e8f4f06b96839e
JAYSHREE BAWANKAR <jayshreebawankar27@gmail.com>
2025-02-21 12:25:41 +0530

    declare winner UI done

--- app/admin/contest/winner-declare/WinnerDeclare.js
+++ app/admin/contest/winner-declare/WinnerDeclare.js
@@ -12,6 +12,7 @@ export default function PlayerSearch() {
     const [winners, setWinners] = useState([]);
     const [players, setPlayers] = useState([]);
     const [loader, setLoader] = useState(false);
+    const [contest, setContest] = useState(null);
     const [isMounted, setIsMounted] = useState(false);
     const searchParams = useSearchParams();
     const router = useRouter();
@@ -34,15 +35,56 @@ export default function PlayerSearch() {
         }
     }
 
+    async function getContestDetails() {
+        try {
+            setLoader(true);
+            const serverResponse = await adminCommunication.getContestDetails(searchParams.get("contestId"));
+            if (serverResponse.data.status === "SUCCESS") {
+                setContest(serverResponse?.data?.contest);
+            } else if (serverResponse?.data?.status === "JWT_INVALID") {
+                Swal.fire({ text: serverResponse?.data?.message, icon: "warning" });
+                router.push("/");
+            } else {
+                Swal.fire({ text: serverResponse?.data?.message, icon: "warning" });
+            }
+        } catch (error) {
+            Swal.fire({ text: error?.response?.data?.message || error.message, icon: "warning" });
+        } finally {
+            setLoader(false);
+        }
+    }
     useEffect(() => {
         setIsMounted(true);
+        getContestDetails()
         getContestWisePlayerList();
     }, []);
 
     // Function to declare the winners
-    const handleDeclareWinner = () => {
+    const handleDeclareWinner = async () => {
         if (selectedPlayers.length > 0) {
-            setWinners(selectedPlayers.map(player => player.label)); // Officially set the winners
+            try {
+                setLoader(true);
+                const payload = {
+                    contestId: searchParams.get("contestId"), // Pass contestId in the payload
+                    playerId: "ee1ff423-c6b4-4649-b9f1-4ebfb467cf9f"
+                    // playerIds: selectedPlayers.map(player => player.label)
+                };
+
+                const serverResponse = await adminCommunication.declareWinner(payload);
+                if (serverResponse?.data?.status === "SUCCESS") {
+                    // setGameWiseContestsList(serverResponse?.data?.contestList);
+                    setWinners(selectedPlayers.map(player => player.label)); // Officially set the winners
+                } else if (serverResponse?.data?.status === "JWT_INVALID") {
+                    Swal.fire({ text: serverResponse?.data?.message, icon: "warning" });
+                    router.push("/");
+                } else {
+                    Swal.fire({ text: serverResponse?.data?.message, icon: "warning" });
+                }
+            } catch (error) {
+                Swal.fire({ text: error?.response?.data?.message || error?.message, icon: "warning" });
+            } finally {
+                setLoader(false);
+            }
         } else {
             Swal.fire({ text: "Please select at least one winner first!", icon: "warning" });
         }
@@ -55,7 +97,7 @@ export default function PlayerSearch() {
                 :
                 <div style={{ width: "90%", margin: "0px auto" }}>
                     <div className="mt-1 header-container">
-                        <p className="tournament_text">DECLARE WINNERS</p>
+                        <p className="tournament_text">{contest?.name ? `${contest?.name}` : `DECLARE WINNERS`}</p>
                         <button type="button" className="btn btn-outline-light back_btn" onClick={() => router.back()}> Back </button>
                     </div>
 
@@ -113,7 +155,7 @@ export default function PlayerSearch() {
                                     fontWeight: "bold",
                                     transition: "all 0.3s ease",
                                 }}
-                                onClick={handleDeclareWinner}
+                                onClick={() => handleDeclareWinner()}
                             >
                                 Declare Winners
                             </button>
